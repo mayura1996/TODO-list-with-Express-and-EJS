32466cb3371c0e018cd98e57c525d423b4b33fce
MAYURA MANAWADU <mmayurapavan@gmail.com>
2021-01-20 00:02:06 +0530

    final commit

--- app.js
+++ app.js
@@ -5,11 +5,14 @@
 
  const app = express();
 
+ let items = ["Buy Food", "Cook Food", "Eat Food"];
+ let workItems = [];
  app.set('view engine', 'ejs');
 
  app.use(bodyParser.urlencoded({
    extended: true
  }));
+ app.use(express.static("public"));
 
  app.get("/", function(req, res) {
 
@@ -23,16 +26,40 @@
    var day = today.toLocaleDateString("en-US", options);
 
    res.render("list", {
-     kindOfDay: day
+     listTitle: day,
+     newListItems: items
    });
-   app.post("/", function(req, res) {
-     var item=req.body.newItem;
-     comsole.log(item)
-
-   })
 
  });
 
+ app.post("/", function(req, res) {
+   var item = req.body.newItem;
+   if (req.body.list == "Work") {
+     workItems.push(item);
+     res.redirect("/work");
+   } else {
+     items.push(item);
+     res.redirect("/");
+   }
+
+ })
+
+ app.get("/work", function(req, res) {
+   res.render("list", {
+     listTitle: "Work List",
+     newListItems: workItems
+   });
+ });
+
+ app.post("/work", function(req, res) {
+   let item = req.body.newItem;
+   workItems.push(item);
+   res.redirect("/work")
+ })
+
+app.get("/about",function(req,res){
+  res.render("about");
+})
 
 
  app.listen(3000, function() {
